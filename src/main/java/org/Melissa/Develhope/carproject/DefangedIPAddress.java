b25f06281d9a2c048b608456ea507a74817a5393
Feza-coder <fezamukundwamelissa@gmail.com>
2024-10-11 09:35:17 +0300

    Defanged IP address Exercise

--- src/main/java/org/Melissa/Develhope/carproject/DefangedIPAddress.java
+++ src/main/java/org/Melissa/Develhope/carproject/DefangedIPAddress.java
@@ -1,2 +1,17 @@
-package org.Melissa.Develhope.carproject;public class DefangedIPAddress {
+package org.Melissa.Develhope.carproject;
+
+// Given a valid (IPv4) IP address, return a defanged version of that IP address.
+
+// A defanged IP address replaces every period "." with "[.]".
+
+public class DefangedIPAddress {
+    public static String defangIPaddr(String address) {
+        return address.replace(".", "[.]");
+    }
+    public static void main(String[] args) {
+        String ipAddress = "182.192.1.6";
+        String defangedAddress = defangIPaddr(ipAddress);
+        System.out.println("Original IP Address: " + ipAddress);
+        System.out.println("Defanged IP Address: " + defangedAddress);
+    }
 }
